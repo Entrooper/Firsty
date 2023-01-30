d2093a283656bb85105ffa1a8849dcc0c8e95934
entrooper <mnarlioglu@gmail.com>
2023-01-31 01:37:05 +0300

    first commit

---
+++ Firsty/ContentView.swift
@@ -0,0 +1,85 @@
+//
+//  ContentView.swift
+//  Firsty
+//
+//  Created by Mehmet NARLIOGLU on 28.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+    
+    var body: some View {
+        ZStack {
+            mainColor.ignoresSafeArea()
+            VStack {
+               Text("1/10")
+                    .font(.callout)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                Text("What was the first computer bug?")
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                Spacer()
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        Text("Ant")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                        
+                    }
+                    Button {
+                        
+                    } label: {
+                        Text("Beetle")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                        
+                    }
+                    Button {
+                        
+                    } label: {
+                        Text("Moth")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                        
+                    }
+                    Button {
+                        
+                    } label: {
+                        Text("Fly")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                        
+                    }
+
+                }
+            }
+            .foregroundColor(Color.white)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Firsty/FirstyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstyApp.swift
+//  Firsty
+//
+//  Created by Mehmet NARLIOGLU on 28.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

9946bc0dfdcc45719906d4f73f4862863dafec46
entrooper <mnarlioglu@gmail.com>
2023-02-04 20:48:21 +0300

    added-Game-Model

---
+++ Firsty/Models/Question.swift
@@ -0,0 +1,35 @@
+//
+//  Question.swift
+//  Firsty
+//
+//  Created by Mehmet NARLIOGLU on 2.02.2023.
+//
+
+import Foundation
+
+
+struct Question: Hashable {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+    
+    static var allQuestions = [
+        Question(questionText: "Who invented the World Wide Web?",
+                 possibleAnswers: [
+                    "Steve Jobs",
+                    "Linus Torvalds",
+                    "Bill Gates",
+                    "Tim Berners-Lee"
+                 ],
+                 correctAnswerIndex: 3),
+        Question(questionText: "What was the first object oriented programming language?",
+                 possibleAnswers: [
+                    "Simula",
+                    "Python",
+                    "Swift",
+                    "C"
+                 ],
+                 correctAnswerIndex: 0)
+    ]
+    
+}
